1ff9ea955dbb7a8be67eaae55955b76d47206023
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-20 16:00:45 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h2>Home page</h2>
+        </div>
+    );
+};
+
+export default Home;
