8978126ec0d1f3afec49b19b0fa64e2c393661cb
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-23 21:07:32 +0600

    nav e hight set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import Nav from "../components/Nav";
 const Home = () => {
     return (
         <div>
+            <div className="h-16">
             <Nav></Nav>
+            </div>
             <h2>Home page</h2>
         </div>
     );
