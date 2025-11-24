b4db065345c0ed0d1ac064b947948294c2b51749
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-24 21:14:57 +0600

    createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+  }
+]);
+
+createRoot(document.getElementById("root")).render(
   <>
-    <App />
-  </>,
-)
+    <RouterProvider router={router}></RouterProvider>
+    {/* <App/> */}
+  </>
+);
