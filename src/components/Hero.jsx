1155d78d8f15fdae1b266aaf6bd07b9b50739272
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-28 22:24:46 +0600

    class change in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <div className="flex">  
+          <div className="flex gap-8">  
             <button className="btn btn-primary">Read Blogs</button>
             <button className="btn btn-primary">Bookmarks</button>
           </div>
