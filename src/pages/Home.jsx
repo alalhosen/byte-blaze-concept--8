dc99bd203a69092c7784d259d3268b18c75c7292
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-29 22:53:44 +0600

    calc set in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Nav from "../components/Nav";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
       <Hero></Hero>
     </div>
   );
