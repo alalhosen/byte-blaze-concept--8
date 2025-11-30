0f8a1d42cc65f60791a21664f4a1dcf69cdba43c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-30 20:42:20 +0600

    btn change & set Link in hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <div className="hero bg-base-200">
       <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Welcome To ByteBlaze</h1>
+        <div className="max-w-lg">
+          <h1 className="text-5xl font-bold">
+            Welcome To <span>ByteBlaze</span>
+          </h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <div className="flex gap-8">  
-            <button className="btn btn-primary">Read Blogs</button>
-            <button className="btn btn-primary">Bookmarks</button>
+          <div className="flex gap-2 justify-center">
+            <Link to='/blogs'>Read Blogs</Link>
+            <Link to='/bookmarks'>Bookmarks</Link>
           </div>
         </div>
       </div>
