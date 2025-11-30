4cb5de2a5c0db8bcd9d821a94b3b48a8038c13f0
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-30 23:57:48 +0600

    some change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,11 +25,11 @@ const Hero = () => {
               </span>
             </Link>
             <Link
-              to='/bookmarks'
+              to="/bookmarks"
               className="relative inline-block px-4 py-2 font-medium group"
             >
-              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-              <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
               <span className="relative text-black group-hover:text-white">
                 Bookmarks
               </span>
