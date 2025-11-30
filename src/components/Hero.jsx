1e8d6836bb1173186b1df2a1c1ec80e73c86f2bf
Alal Hosen <alalhosen1992620120@gmail.com>
2025-11-30 23:56:06 +0600

    a change to link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,8 +14,26 @@ const Hero = () => {
             a id nisi.
           </p>
           <div className="flex gap-2 justify-center">
-            <Link to='/blogs'>Read Blogs</Link>
-            <Link to='/bookmarks'>Bookmarks</Link>
+            <Link
+              to="/blogs"
+              className="relative inline-block px-4 py-2 font-medium group"
+            >
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+              <span className="relative text-black group-hover:text-white">
+                Read Blogs
+              </span>
+            </Link>
+            <Link
+              to='/bookmarks'
+              className="relative inline-block px-4 py-2 font-medium group"
+            >
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+              <span className="relative text-black group-hover:text-white">
+                Bookmarks
+              </span>
+            </Link>
           </div>
         </div>
       </div>
