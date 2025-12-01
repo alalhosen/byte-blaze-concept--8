8d32a480277f8e58c462c2a04b1eae8ec3320948
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-01 23:36:30 +0600

    animated svg class set

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero";
-import Nav from "../components/Nav";
+// import Nav from "../components/Nav";
+import wave from "../assets/wave.svg";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)]">
+    <div className="flex relative flex-col items-center justify-center min-h-[calc(100vh-116px)]">
       <Hero></Hero>
+      <img className="absolute rotate-180  bottom-0" src={wave} alt="" />
     </div>
   );
 };
 
-export default Home; 
+export default Home;
