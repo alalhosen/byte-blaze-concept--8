e3ab887e702d362d047ee9e63084a5b4e1510161
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-04 16:28:57 +0600

    set fetch

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,39 +1,39 @@
-import { useEffect, useState } from "react";
 import Nav from "../components/Nav";
-import { data } from "react-router-dom";
+import { data, useLoaderData } from "react-router-dom";
 
 const Blogs = () => {
-const [blogs, setBlogs]=useState([])
-useEffect(()=>{
-  fetch('https://dev.to/api/articles?per_page=7&top=7')
-  .then(res=>res.json())
-  .then(data=>setBlogs(data))
-},[])
+  // const [blogs, setBlogs]=useState([])
+  // useEffect(()=>{
+  //   fetch('https://dev.to/api/articles?per_page=7&top=7')
+  //   .then(res=>res.json())
+  //   .then(data=>setBlogs(data))
+  // },[])
+  // console.log(data)
+
+  const blogs = useLoaderData();
+  console.log(blogs);
 
   return (
     <section className="">
-      <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+      <div className="container max-w-6xl p-6 py-6 mx-auto space-y-6 sm:space-y-12">
         <a
           rel="noopener noreferrer"
           href="#"
           className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50"
         >
           <img
-            src="https://source.unsplash.com/random/480x360"
+            src={blogs[0].cover_image}
             alt=""
             className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500"
           />
           <div className="p-6 space-y-2 lg:col-span-5">
             <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
-              Noster tincidunt reprimique ad pro
+              {blogs[0].title}
             </h3>
             <span className="text-xs dark:text-gray-600">
-              February 19, 2021
+        {new Date(blogs[0].published_at).toLocaleDateString()}
             </span>
-            <p>
-              Ei delenit sensibus liberavisse pri. Quod suscipit no nam. Est in
-              graece fuisset, eos affert putent doctus id.
-            </p>
+            <p>{blogs[0].description}</p>
           </div>
         </a>
         <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
