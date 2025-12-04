69667c96a93a156d1d0e0b5b2e76478a433a27c4
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-04 23:54:56 +0600

    single blog set

--- src/main.jsx
+++ src/main.jsx
@@ -6,26 +6,33 @@ import Home from "./pages/Home.jsx";
 import Blogs from "./pages/Blogs.jsx";
 import Bookmarks from "./pages/Bookmarks.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
+import Blog from "./pages/Blog.jsx";
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<MainLayout></MainLayout>,
-    children:[
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
       {
-        path:'/',
-        element:<Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path:'/blogs',
-        element:<Blogs></Blogs>,
-        loader:()=>fetch('https://dev.to/api/articles?per_page=15&top=7')
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+        loader: () => fetch("https://dev.to/api/articles?per_page=15&top=7"),
       },
       {
-        path:'/bookmarks',
-        element:<Bookmarks></Bookmarks>
-      }
-    ]
+        path: "/blog/:id",
+        element: <Blog></Blog>,
+        loader: ({ params }) =>
+          fetch(`https://dev.to/api/articles/${params.id}`),
+      },
+      {
+        path: "/bookmarks",
+        element: <Bookmarks></Bookmarks>,
+      },
+    ],
   },
   // {
   //   path:'/blogs',
