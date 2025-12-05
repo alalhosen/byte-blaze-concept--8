3b682ab523f41ca0378999b64d9fcd47b26b1f14
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-05 14:46:57 +0600

    Tabs set in blog page

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,9 +1,14 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Blog = () => {
   const blog = useLoaderData();
-  const { comments_count, title, reading_time_minutes, public_reactions_count,published_at } =
-    blog;
+  const {
+    comments_count,
+    title,
+    reading_time_minutes,
+    public_reactions_count,
+    published_at,
+  } = blog;
   return (
     <div className="max-w-3xl px-6 py-16 mx-auto space-y-12">
       <article className="space-y-8 dark:bg-gray-100 dark:text-gray-900">
@@ -12,16 +17,99 @@ const Blog = () => {
             {title}
           </h1>
           <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center dark:text-gray-600">
-            <p className="text-sm">{reading_time_minutes} min read . {''}{new Date(published_at).toLocaleDateString()}</p>
+            <p className="text-sm">
+              {reading_time_minutes} min read . {""}
+              {new Date(published_at).toLocaleDateString()}
+            </p>
 
             <p className="flex-shrink-0 mt-3 text-sm md:mt-0">
-           {comments_count} comments • {public_reactions_count} view
+              {comments_count} comments • {public_reactions_count} view
             </p>
           </div>
+
+          {/********  Tabs **********/}
+          <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+            <Link
+            to=''              
+              className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <span>Content</span>
+            </Link>
+            <Link
+              to={`author`}
+              className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+              </svg>
+              <span>Author</span>
+            </Link>
+
+            {/* <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+              </svg>
+              <span>Excepturi</span>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <circle cx="12" cy="12" r="10"></circle>
+                <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
+              </svg>
+              <span>Consectetur</span>
+            </a> */}
+            
+          </div>
         </div>
-        <div className="dark:text-gray-800">
-          <p>Insert the actual text content here...</p>
-        </div>
+       
       </article>
       <div>
         <div className="flex flex-wrap py-6 gap-2 border-t border-dashed dark:border-gray-600">
