b2acc1e0d56ea435cc771ba90e5a791e3694e9b8
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-09 21:48:15 +0600

    set save function in bookmark

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import {RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes";
+import { Toaster } from "react-hot-toast";
 
 
 
@@ -10,5 +11,6 @@ import { router } from "./routes/Routes";
 createRoot(document.getElementById("root")).render(
   <>
     <RouterProvider router={router}></RouterProvider>
+    <Toaster></Toaster>
   </>
 );
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+import toast from "react-hot-toast"
 export const getBlogs=()=>{
     let blogs = []
     const storedBlogs = localStorage.getItem('blogs')
@@ -5,4 +6,13 @@ if (storedBlogs){
     blogs=JSON.parse(storedBlogs)
 }
     return blogs
+}
+
+// save
+export const saveBlog = blog => {
+    let blogs = getBlogs()
+    const isExist = blogs.find(blog => blog.id === blog.id)
+    if (isExist) {
+        return toast.error('Already Bookmarked!')
+    }
 }
