e5a3e2db654d46902d0990e484b73f34964fc1be
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-10 21:49:47 +0600

    install react toast & set

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,26 +1,30 @@
-import toast from "react-hot-toast"
-export const getBlogs=()=>{
-    let blogs = []
-    const storedBlogs = localStorage.getItem('blogs')
-if (storedBlogs){
-    blogs=JSON.parse(storedBlogs)
-}
-    return blogs
-}
+import toast from "react-hot-toast";
+export const getBlogs = () => {
+  let blogs = [];
+  const storedBlogs = localStorage.getItem("blogs");
+  if (storedBlogs) {
+    blogs = JSON.parse(storedBlogs);
+  }
+
+  return blogs;
+};
 
 // save
-export const saveBlog = blog => {
-    let blogs = getBlogs()
-    const isExist = blogs.find(blog => blog.id === blog.id)
-    if (isExist) {
-        return toast.error('Already Bookmarked!')
-    }
-}
+export const saveBlog = (blog) => {
+  let blogs = getBlogs();
+  const isExist = blogs.find((b) => b.id === blog.id);
+  if (isExist) {
+    return toast.error("Already Bookmarked!");
+  }
+  blogs.push(blog);
+  localStorage.setItem("blogs", JSON.stringify(blogs));
+  toast.success("Blog Bookmarked Successfully!");
+};
 
 // delete
-export const deleteBlog = id => {
-    let blogs = getBlogs()
-    const remaining = blogs.filter(blog => blog.id !== id)
-    localStorage.setItem('blogs', JSON.stringify(remaining))
-    toast.success('Blog Removed from Bookmark')
-}
+export const deleteBlog = (id) => {
+  let blogs = getBlogs();
+  const remaining = blogs.filter((b) => b.id !== id);
+  localStorage.setItem("blogs", JSON.stringify(remaining));
+  toast.success("Blog Removed from Bookmark");
+};
