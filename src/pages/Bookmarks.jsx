7b5f9d8796f697405b63dfdb514ca453b5214444
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-11 21:01:34 +0600

    delete btn set & rendering in bookmark

--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -5,15 +5,17 @@ import BlogCard from "../components/BlogCard";
 const Bookmarks = () => {
   const [blogs, setBlogs] = useState([]);
   useEffect(() => {
-    const storedBlogs = getBlogs()
-    setBlogs(storedBlogs)
+    const storedBlogs = getBlogs();
+    setBlogs(storedBlogs);
   }, []);
-  return;
-  <div className="grid px-4 sm:px-8 lg:px12 py-8 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-    {blogs.map((blog) => (
-      <BlogCard blog={blog} key={blog.id} />
-    ))}
-  </div>;
+
+  return (
+    <div className="grid px-4 sm:px-8 lg:px12 py-8 justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {blogs.map((blog) => (
+        <BlogCard deletable={true} blog={blog} key={blog.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Bookmarks;
