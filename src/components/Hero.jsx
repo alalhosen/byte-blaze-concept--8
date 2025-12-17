9ccbe35ca00dafedaa05f68de92bddfe61ed2a2c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-12-17 21:38:07 +0600

    new link & btn add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,6 +34,16 @@ const Hero = () => {
                 Bookmarks
               </span>
             </Link>
+            <Link
+              to="/bookmarks"
+              className="relative inline-block ml-4 px-4 py-2 font-medium group"
+            >
+              <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+              <span className="relative text-black group-hover:text-white">
+               Contents
+              </span>
+            </Link>
           </div>
         </div>
       </div>
